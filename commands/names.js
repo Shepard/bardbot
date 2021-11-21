0fc16a17b9601557ac8185d5a7b0a7cd1b115d54
Simon Reinhardt <Shepard@users.noreply.github.com>
2021-11-21 21:43:10 +0100

    Limit length of /names reply
    If the reply to /names would exceed Discord's limit for embed descriptions, split it up into several follow-up messages.

--- commands/names.js
+++ commands/names.js
@@ -1,6 +1,8 @@
 import { userMention } from '@discordjs/builders';
 import { Constants, MessageEmbed } from 'discord.js';
 
+const CHARACTER_LIMIT = 4096;
+
 const namesCommand = {
 	// Configuration for registering the command
 	configuration: {
@@ -15,20 +17,30 @@ const namesCommand = {
 
 		// Retrieve all (non-bot) members of this guild and add their names to the list.
 		let messageText = '';
+		const messages = [];
 		const members = await interaction.guild.members.fetch();
 		members.each(member => {
 			if (!member.user.bot && member.user.username !== member.displayName) {
-				if (messageText) {
-					messageText += '\n';
+				const newLine = `${member.displayName} - ${userMention(member.id)}`;
+				// If messageText would exceed the character limit of embed descriptions by appending this line, split it off into a separate message.
+				if (messageText.length + newLine.length + 1 /*line break*/ > CHARACTER_LIMIT) {
+					messages.push(messageText);
+					messageText = newLine;
+				} else {
+					if (messageText) {
+						messageText += '\n';
+					}
+					messageText += newLine;
 				}
-				messageText += `${member.displayName} - ${userMention(member.id)}`;
 			}
 		});
+		messages.push(messageText);
 
 		// Send member list as an embed message, editing the original deferred reply.
+		// The initial message contains a title and the first batch of names.
 		const embed = new MessageEmbed()
 			.setTitle(`When we turn the back the pages, the members of ${interaction.guild.name} had the following names:`)
-			.setDescription(messageText);
+			.setDescription(messages[0]);
 		await interaction.editReply({
 			embeds: [embed],
 			// Suppress mentions because we don't want to ping people.
@@ -36,6 +48,17 @@ const namesCommand = {
 				parse: []
 			}
 		});
+
+		// If there are more messages needed, send them as follow-ups.
+		for (let i = 1; i < messages.length; i++) {
+			const followUpEmbed = new MessageEmbed().setDescription(messages[i]);
+			await interaction.followUp({
+				embeds: [followUpEmbed],
+				allowed_mentions: {
+					parse: []
+				}
+			});
+		}
 	}
 };
 
