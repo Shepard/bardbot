cc3246f628afdbbc3f54e116c82da6e52edf0fee
Simon Reinhardt <Shepard@users.noreply.github.com>
2021-11-27 01:04:14 +0100

    Further improved handling of many users for /names.
    Before sending a new message, add more embeds to the current one.

--- commands/names.js
+++ commands/names.js
@@ -1,6 +1,8 @@
 import { userMention } from '@discordjs/builders';
 import { Constants, MessageEmbed } from 'discord.js';
 
+// Limit for characters in the description of an embed.
+// See https://discord.com/developers/docs/resources/channel#embed-limits
 const CHARACTER_LIMIT = 4096;
 
 const namesCommand = {
@@ -17,14 +19,14 @@ const namesCommand = {
 
 		// Retrieve all (non-bot) members of this guild and add their names to the list.
 		let messageText = '';
-		const messages = [];
+		const messageTexts = [];
 		const members = await interaction.guild.members.fetch();
 		members.each(member => {
 			if (!member.user.bot && member.user.username !== member.displayName) {
 				const newLine = `${member.displayName} - ${userMention(member.id)}`;
-				// If messageText would exceed the character limit of embed descriptions by appending this line, split it off into a separate message.
+				// If messageText would exceed the character limit of embed descriptions by appending this line, split it off into a separate messageText.
 				if (messageText.length + newLine.length + 1 /*line break*/ > CHARACTER_LIMIT) {
-					messages.push(messageText);
+					messageTexts.push(messageText);
 					messageText = newLine;
 				} else {
 					if (messageText) {
@@ -34,32 +36,39 @@ const namesCommand = {
 				}
 			}
 		});
-		messages.push(messageText);
+		messageTexts.push(messageText);
 
-		// Send member list as an embed message, editing the original deferred reply.
-		// The initial message contains a title and the first batch of names.
-		const embed = new MessageEmbed()
-			.setTitle(`When we turn the back the pages, the members of ${interaction.guild.name} had the following names:`)
-			.setDescription(messages[0]);
-		await interaction.editReply({
-			embeds: [embed],
+		// Send member list as messages with embeds. The initial message contains a title and the first batch of names and edits the original deferred reply.
+
+		const embeds = messageTexts.map(text => new MessageEmbed().setDescription(text));
+		embeds[0].setTitle(`When we turn the back the pages, the members of ${interaction.guild.name} had the following names:`);
+
+		// We can send up to ten embeds per message: https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-messages
+		const embedChunks = chunk(embeds, 10);
+
+		const messages = embedChunks.map(embedChunk => ({
+			embeds: embedChunk,
 			// Suppress mentions because we don't want to ping people.
 			allowed_mentions: {
 				parse: []
 			}
-		});
+		}));
+
+		await interaction.editReply(messages[0]);
 
 		// If there are more messages needed, send them as follow-ups.
 		for (let i = 1; i < messages.length; i++) {
-			const followUpEmbed = new MessageEmbed().setDescription(messages[i]);
-			await interaction.followUp({
-				embeds: [followUpEmbed],
-				allowed_mentions: {
-					parse: []
-				}
-			});
+			await interaction.followUp(messages[i]);
 		}
 	}
 };
+
+function chunk(items, chunkSize) {
+	const chunked = [];
+	for (let i = 0; i < items.length; i += chunkSize) {
+		chunked.push(items.slice(i, i + chunkSize));
+	}
+	return chunked;
+}
 
 export default namesCommand;
