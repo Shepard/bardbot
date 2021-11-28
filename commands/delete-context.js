e6b1a16d6c9e07c4bb8ec6dffb8db906d79dca9f
Simon Reinhardt <Shepard@users.noreply.github.com>
2021-11-28 12:28:57 +0100

    Don't check mentions in /names reply for delete command.
    Since the /names reply also mentions an arbitrary user in the last line of it's embed's description we have to make sure that embed isn't checked for mentions in the delete command.
    Also combined two ifs and fixed the position of an optional chaining operator in the check for embeds.

--- commands/delete-context.js
+++ commands/delete-context.js
@@ -13,33 +13,34 @@ const deleteContextCommand = {
 	async execute(interaction) {
 		// Get the message that the context menu command was used on.
 		const message = interaction.options.getMessage('message');
-		if (message) {
-			if (message.author.id === interaction.client.user.id) {
-				// This is a message that was created by this bot's user.
+		if (message?.author.id === interaction.client.user.id) {
+			// This is a message that was created by this bot's user.
 
-				const commandName = message.interaction?.commandName;
-				if (
-					(commandName === 'bookmark' || commandName === 'names') &&
-					message.interaction?.user.id === interaction.user.id
-				) {
-					// This was the bot's reply to a /bookmark or /names command interaction
-					// and it was used by the current user so we allow it to be deleted.
+			const commandName = message.interaction?.commandName;
+			if (
+				(commandName === 'bookmark' || commandName === 'names') &&
+				message.interaction?.user.id === interaction.user.id
+			) {
+				// This was the bot's reply to a /bookmark or /names command interaction
+				// and it was used by the current user so we allow it to be deleted.
+				await deleteMessage(message, interaction);
+				return;
+			} else if (message.embeds?.length && message.embeds[0].description && commandName !== 'names') {
+				// We're excluding interaction replies to /names here so that the last line of the embed description
+				// of that is not checked for mentions (because it can contain an arbitrary user).
+
+				const lines = message.embeds[0].description.split('\n');
+				const firstLine = lines[0];
+				const lastLine = lines[lines.length - 1];
+
+				// Check if there's a user mention in the first or last line of the embed's description.
+				// So they're likely either the person being quoted (first line)
+				// and thus have a right for this quote to be removed for the sake of fairness.
+				// Or they're the creator of the quote or bookmark (last line)
+				// and thus definitely should be able to remove it.
+				if (isUserMentioned(firstLine, interaction.user.id) || isUserMentioned(lastLine, interaction.user.id)) {
 					await deleteMessage(message, interaction);
 					return;
-				} else if (message?.embeds.length && message.embeds[0].description) {
-					const lines = message.embeds[0].description.split('\n');
-					const firstLine = lines[0];
-					const lastLine = lines[lines.length - 1];
-
-					// Check if there's a user mention in the first or last line of the embed's description.
-					// So they're likely either the person being quoted (first line)
-					// and thus have a right for this quote to be removed for the sake of fairness.
-					// Or they're the creator of the quote or bookmark (last line)
-					// and thus definitely should be able to remove it.
-					if (isUserMentioned(firstLine, interaction.user.id) || isUserMentioned(lastLine, interaction.user.id)) {
-						await deleteMessage(message, interaction);
-						return;
-					}
 				}
 			}
 		}
